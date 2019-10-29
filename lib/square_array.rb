18ad7b29a9295f103d09108f3847badbdbfa4849
Brian Nicholls <nichol88@gmail.com>
2019-10-29 05:46:51 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  i = 0
+  while (i<array.length)
+    array[i] = array[i] ** 2
+    i += 1
+
+  end
+  return array
 end
--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,5 +1,6 @@
 require_relative './spec_helper'
 
+
 describe '#square_array' do
 
   it 'does not call on each, collect, map, or inject' do
